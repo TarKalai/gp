316c8edd2fd6e595c7841d7e18bda18f987f289e
TarKalai <tarick.kalai@gmail.com>
2025-05-24 15:30:34 +0200

    computing fibonnaci sequence

--- src/gp/main.py
+++ src/gp/main.py
@@ -1,5 +1,15 @@
+def fibonacci_recursive(n: int) -> int:
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
 def main() -> None:
-    print("Hello from gp!")
+    result = fibonacci_recursive(13)
+    print("Hello from gp!: ", result)
 
 
 if __name__ == "__main__":
