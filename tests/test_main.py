0c8d1093bfc443c367137a6a356970ea70abef63
TarKalai <tarick.kalai@gmail.com>
2025-05-24 15:43:50 +0200

    Test for fibonacci

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,10 @@
+from gp.main import fibonacci_recursive
+
+
 def test_main() -> None:
     assert True
+
+
+def test_fibonacci_recursive() -> None:
+    expected_answer = 233
+    assert fibonacci_recursive(13) == expected_answer
